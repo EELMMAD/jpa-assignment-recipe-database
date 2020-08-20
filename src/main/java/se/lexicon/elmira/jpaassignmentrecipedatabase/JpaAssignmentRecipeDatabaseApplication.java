994bb9c6e5f0b370f5fdf50167c430bd737b5a43
Elmira Madadi <elmiramadadi@gmail.com>
2020-08-20 21:45:59 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/elmira/jpaassignmentrecipedatabase/JpaAssignmentRecipeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.elmira.jpaassignmentrecipedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssignmentRecipeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAssignmentRecipeDatabaseApplication.class, args);
+	}
+
+}
